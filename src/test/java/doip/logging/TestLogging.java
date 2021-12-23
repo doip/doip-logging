419f959939cd3f47e804fb398d77c1c053b64a73
Florian Roks <froks@users.noreply.github.com>
2021-12-23 22:14:59 +0100

    - Upgrade log4j to a non-vulnerable version (2.17) - Upgrade gradle to a newer version that supports newer JDKs - Switched jcenter to mavenCentral, since jcenter is sunset for march 2022

--- src/test/java/doip/logging/TestLogging.java
+++ src/test/java/doip/logging/TestLogging.java
@@ -1,5 +1,4 @@
 package doip.logging;
-import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -7,13 +6,10 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import doip.logging.LogManager;
-import doip.logging.Logger;
-
 public class TestLogging {
-	
+
 	private static Logger logger = LogManager.getLogger(TestLogging.class);
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
